e95d02bbfc0edda6f8d82e6d292cac253581c0ca
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-01-01 11:47:07 +0200

    Do not export internal symbols

--- parse.h
+++ parse.h
@@ -1,8 +1,8 @@
 #ifndef C2ACEA4D_143C_422B_BD20_ED67437A4CA9
 #define C2ACEA4D_143C_422B_BD20_ED67437A4CA9
 
-#include <Zend/zend.h>
+#include "php_dotenv.h"
 
-void parse_file(const char* fname, zval* zcontext, HashTable* res);
+DOTENV_VISIBILITY_HIDDEN void parse_file(const char* fname, zval* zcontext, HashTable* res);
 
 #endif /* C2ACEA4D_143C_422B_BD20_ED67437A4CA9 */
--- utils.h
+++ utils.h
@@ -1,9 +1,9 @@
 #ifndef D5A15088_DB24_46C5_8874_73F80B952A1E
 #define D5A15088_DB24_46C5_8874_73F80B952A1E
 
-#include <Zend/zend.h>
+#include "php_dotenv.h"
 
-zend_string* find_file_upward(const zend_string* dir, const zend_string* name);
-zend_string* cli_find_env_file(const char* filename, zend_bool use_cwd);
+DOTENV_VISIBILITY_HIDDEN zend_string* find_file_upward(const zend_string* dir, const zend_string* name);
+DOTENV_VISIBILITY_HIDDEN zend_string* cli_find_env_file(const char* filename, zend_bool use_cwd);
 
 #endif /* D5A15088_DB24_46C5_8874_73F80B952A1E */
